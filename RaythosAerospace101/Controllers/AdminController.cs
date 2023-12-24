e6a27b4fdb7a709384c88e6fb2f062e4ece9c27e
vishva-kalhara <vishvakalhara@gmail.com>
2023-12-24 17:29:41 +0530

    plane management for admin

--- RaythosAerospace101/Controllers/AdminController.cs
+++ RaythosAerospace101/Controllers/AdminController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RaythosAerospace101.Data;
 using RaythosAerospace101.Models;
+using RaythosAerospace101.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -35,8 +37,53 @@ namespace RaythosAerospace101.Controllers
             return View(objList);
         }
         
-        public IActionResult ManageOrders()
+        public IActionResult AeroplaneOrders()
         {
+            //if (HttpContext.Session.GetString("role") != "4")
+            //{
+            //    return RedirectToAction("OnlyUsers", "Messages");
+            //}
+            IEnumerable<CustomizedPlane> objList = _db.CustomizedPlanes
+                    .Include(cp => cp.Plane)
+                    .Include(cp => cp.OverallStatus)
+                    .Include(cp => cp.PlaneDesignScheme)
+                    .Include(cp => cp.FloorPlan)
+                    .Where(cp => cp.IsBasket == false)
+                    .Where(cp => cp.OverallStatusId > 1)
+                    .ToList();
+            return View(objList);
+        }
+
+        public IActionResult FinishStage(int id)
+        {
+            //if (HttpContext.Session.GetString("role") != "4")
+            //{
+            //    return RedirectToAction("OnlyUsers", "Messages");
+            //}
+            var currObj = _db.CustomizedPlanes.Find(id);
+            if(currObj.OverallStatusId < 5)
+            {
+                currObj.OverallStatusId++;
+            }
+            _db.CustomizedPlanes.Update(currObj);
+            _db.SaveChanges();
+            return RedirectToAction("AeroplaneOrders", "Admin");
+        }
+
+
+        public IActionResult OtherOrders()
+        {
+            //if (HttpContext.Session.GetString("role") != "4")
+            //{
+            //    return RedirectToAction("OnlyUsers", "Messages");
+            //}
+            //IEnumerable<CustomizedPlane> objList = _db.CustomizedPlanes
+            //        .Include(cp => cp.Plane)
+            //        .Include(cp => cp.OverallStatus)
+            //        .Include(cp => cp.PlaneDesignScheme)
+            //        .Include(cp => cp.FloorPlan)
+            //        .Where(cp => cp.IsBasket == false)
+            //        .ToList();
             return View();
         }
 
---
+++ RaythosAerospace101/ViewModels/CustomizedPlane__InvInvoice.cs
@@ -0,0 +1,14 @@
+﻿using RaythosAerospace101.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaythosAerospace101.ViewModels
+{
+    public class CustomizedPlane__InvInvoice
+    {
+        public List<CustomizedPlane> CustomizedPlanes { get; set; }
+        public List<InvInvoice> InvInvoices { get; set; }
+    }
+}
